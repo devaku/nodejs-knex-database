9e295b4e801d75793ce245fdb5ef91191752e3cc
devaku-desktop <alejokimuy@gmail.com>
2020-06-13 08:31:37 +0800

    Added controllers and lib_modules

--- api/controllers/homeController.js
+++ api/controllers/homeController.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const storeModule = require('../../lib_modules/store.js');
 
 router.get('/', async function (req, res) {
     res.render('home', {
         title: 'THIS IS A TITLE',
         message: 'THIS IS A MESSAGE',
     });
+});
+
+router.post('/createuser', express.json(), async function POST_createuser(
+    req,
+    res
+) {
+    try {
+        console.log('');
+        console.log('homecontroller.POST_createuser');
+        let { username, password } = req.body;
+        await storeModule.CreateUser({
+            username,
+            password,
+        });
+        res.status(200).json({
+            status: 'success',
+        });
+    } catch (e) {
+        console.log('');
+        console.log('homecontroller.POST_createuser ERROR');
+        console.log(e);
+        res.json({
+            status: 'error',
+            message: e.message,
+        });
+    }
 });
 
 module.exports = {
