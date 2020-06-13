d51203114565c4b8a053b65f9d1423b40408898b
devaku-desktop <alejokimuy@gmail.com>
2020-06-13 09:39:13 +0800

    Setup knex

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+module.exports = {
+    development: {
+        client: 'mysql',
+        connection: {
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'knex_database',
+        },
+    },
+
+    staging: {
+        client: 'mysql',
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+        },
+    },
+
+    production: {
+        client: 'mysql',
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: 'knex_migrations',
+        },
+    },
+};
--- lib_modules/store.js
+++ lib_modules/store.js
@@ -1,6 +1,10 @@
+const knex = require('knex')(require('../knexfile.js').development);
 module.exports = {
     CreateUser: function CreateUser({ username, password }) {
         console.log(`Add user ${username} with password ${password}`);
-        return Promise.resolve();
+        return knex('user').insert({
+            username,
+            password,
+        });
     },
 };
